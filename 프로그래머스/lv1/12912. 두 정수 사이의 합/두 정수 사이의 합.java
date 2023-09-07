ad4d594e75f9cfcff3d2c748fa908ca47a0e9ff8
Jun-Hyeok Sin <tjdtls690@gmail.com>
2023-09-07 22:37:15 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 3.10 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public long solution(int a, int b) {
+        return LongStream.rangeClosed(Math.min(a, b), Math.max(a, b)).sum();
+    }
+}
