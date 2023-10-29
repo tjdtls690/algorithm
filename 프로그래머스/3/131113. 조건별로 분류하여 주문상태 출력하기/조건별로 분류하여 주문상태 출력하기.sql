c738920fd98c1ed00ba8e9c513d288f958f03628
Jun-Hyeok Sin <tjdtls690@gmail.com>
2023-10-29 19:10:32 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT 
+    ORDER_ID, 
+    PRODUCT_ID, 
+    DATE_FORMAT(OUT_DATE, '%Y-%m-%d'), 
+    case
+        when OUT_DATE > '2022-05-01' then '출고대기'
+        when OUT_DATE <= '2022-05-01' then '출고완료'
+        when OUT_DATE is null then '출고미정'
+    end as 출고여부
+from FOOD_ORDER
+order by ORDER_ID;
